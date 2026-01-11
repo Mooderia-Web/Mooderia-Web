eac772c3a344ed19eb1e5f9b17bbb668c4bf7b94
mooderia <travismiguel08@gmail.com>
2026-01-11 11:33:18 +0000

    Add cache-control meta tags to index.html to reduce stale cached HTML

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './sections/**/*.{js,ts,jsx,tsx}',
+    './**/*.{html,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
